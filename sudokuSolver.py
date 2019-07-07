547f88ebd4786c37e9491d6f6741553052da0cf6
Jonathan <jonathan.vanderwater@gmail.com>
2019-07-07 14:20:21 -0500

    Brute force algorithm in place

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,11 +1,22 @@
 import numpy as np
 import sys
-import copy
+import time
 
 def main():
-    original = Puzzle(open_board(sys.argv[1]))
-    original.print_board()
-    original.solve()
+    if len(sys.argv) == 2:
+        try:
+            original = Puzzle(open_board(sys.argv[1]))
+            print("\nOriginal puzzle: ")
+            original.print_board()
+            original.solve()
+        except FileNotFoundError:
+            print(f"File \"{sys.argv[1]}\" not found.")
+    else:
+        print("Usage: python sudokuSolver.py PUZZLE.txt")
+
+def stop():
+    print("Found solution in %.4f seconds" % (time.time() - start_time))
+    quit()
 
 def open_board(file):
     f = open(file, "r")
@@ -16,7 +27,7 @@ def open_board(file):
 
 class Puzzle:
     def __init__(self, board):
-        self.temp_board = board
+        self.original = board
         self.board = board
         self.board_size = len(self.board)
         self.possible_cell_vals = []
@@ -25,18 +36,16 @@ class Puzzle:
 
     def get_row_vals(self, row):
         return np.asarray(list(filter(lambda a: a != 0, self.board[row,:])))
-
     def get_col_vals(self, col):
         return np.asarray(list(filter(lambda a: a != 0, self.board[:,col])))
-
-    def get_sec_vals(self, sec): # sec is a tuple representing the cartesian coords of the sector from the top left
+    def get_sec_vals(self, sec):
         secList = []
         for i in range(3):
             for j in range(3):
                 secList.append(self.board[sec[1]*3+i][sec[0]*3+j])
         return np.asarray(list(filter(lambda a: a != 0, secList)))
 
-    def get_possible_cell_vals(self, cell): # cell is a tuple representing the cartesian coords of the cell from the top left
+    def get_possible_cell_vals(self, cell):
         row_vals = self.get_row_vals(cell[0])
         col_vals = self.get_col_vals(cell[1])
         sec_vals = self.get_sec_vals((cell[1] // 3, cell[0] // 3))
@@ -59,62 +68,73 @@ class Puzzle:
                     tempRow.append([])
             self.possible_cell_vals.append(np.asarray(tempRow))
 
-    def fill_lonely(self, my_board):
-        for i in range(self.board_size):
-            for j in range(self.board_size):
-                if len(self.possible_cell_vals[i][j]) == 1:
-                    my_board[i][j] = self.possible_cell_vals[i][j][0]
-
-    def solve(self):
-        self.temp_board = self.board;
-        board_copy = np.copy(self.board);
-        while not self.check_solved():
-            if not self.check_lonely():
-                self.fill_lonely(self.board)
-            if np.array_equal(self.board, board_copy):
-                for i in range(len(self.get_possible_cell_vals(self.next_open_cell()))):
-                    if self.solvable_given_assumption(self.next_open_cell(), self.get_possible_cell_vals(self.next_open_cell())[i]):
-                        self.board = self.temp_board;
-            else:
-                board_copy = np.copy(self.board);
-            self.update_possible_cell_vals()
-    
-    def next_open_cell(self):
-        for i in range(self.board_size):
-            for j in range(self.board_size):
-                if self.temp_board[i][j] == 0:
-                    print(i,j)
-                    return (i,j)
-
-    def solvable_given_assumption(self, cell, val):
-        self.temp_board[cell[0]][cell[1]] = val
-        return False
+    def set_cell(self, cell, val):
+        self.board[cell[0]][cell[1]] = val
+        self.update_possible_cell_vals()
 
     def check_lonely(self):
         for i in range(self.board_size):
             for j in range(self.board_size):
                 if len(self.possible_cell_vals[i][j]) == 1:
-                    return False
-        return True
+                    return True
+        return False
+
+    def fill_lonely(self):
+        while self.check_lonely():
+            for i in range(self.board_size):
+                for j in range(self.board_size):
+                    if len(self.possible_cell_vals[i][j]) == 1:
+                        self.set_cell((i, j), self.possible_cell_vals[i][j][0])
+        self.check_solved()
+
+    def solve(self):
+        self.fill_lonely()
+        self.make_assumption(self.next_open_cell())
+
+    def check_for_contradictions(self):
+        for i in range(self.board_size):
+            for j in range(self.board_size):
+                if (len(self.possible_cell_vals[i][j]) == 0) and (self.board[i][j] == 0):
+                    return True
+        return False
+
+    def make_assumption(self, cell):
+        self.check_solved()
+        working_board = np.copy(self.board)
+        for val in self.get_possible_cell_vals(cell):
+            self.board = np.copy(working_board)
+            self.set_cell(cell, val)
+            self.fill_lonely()
+            self.make_assumption(self.next_open_cell())
+            
+    def next_open_cell(self):
+        for i in range(self.board_size):
+            for j in range(self.board_size):
+                if self.board[i][j] == 0:
+                    return (i,j)
 
     def check_solved(self):
         for i in range(self.board_size):
             for j in range(1, 1 + self.board_size):
                 if j not in self.get_row_vals(i):
                     return False
+        print("Solved puzzle:")
         self.print_board()
-        print('Puzzle solved.')
-        return True
+        stop()
 
     def print_board(self):
-        for row in self.board:
-            for char in row:
+        for i, row in enumerate(self.board):
+            for j, char in enumerate(row):
                 if (char != 0):
                     print(char, end="")
                 else:
                     print('.', end="")
-            print() 
-        print()
+                if (j + 1) % 3 == 0:
+                    print(" ", end="")
+            print()
+            if (i + 1) % 3 == 0:
+                print()
 
 if __name__ == "__main__":
+    start_time = time.time()
     main()
